99e762ef83c0fc74a9e17d02f028323e9be6e2ec
José Luis Vallejo Ruiz <hublol58@gmail.com>
2025-09-03 18:55:03 -0500

    Tarea done

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import 'react-native-gesture-handler'; 
+import React from 'react'; 
+      
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createDrawerNavigator } from '@react-navigation/drawer'; 
+import { Provider as PaperProvider } from 'react-native-paper'; 
+ 
+import MisAhorros from './screens/MisAhorros'; 
+import TodosAhorros from './screens/TodosAhorro'; 
+ 
+const Drawer = createDrawerNavigator(); 
+ 
+export default function App() { 
+  return ( 
+    <PaperProvider> 
+      <NavigationContainer> 
+        <Drawer.Navigator initialRouteName="Mis Ahorros"> 
+          <Drawer.Screen name="Mis Ahorros" component={MisAhorros} /> 
+          <Drawer.Screen name="Todos los Ahorros" component={TodosAhorros} /> 
+        </Drawer.Navigator> 
+      </NavigationContainer> 
+    </PaperProvider> 
+  ); 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
